e43b22d22e0ffe115ffaeddfff15ab0d86c2041c
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-01-05 17:21:10 +0500

    update

---
+++ database/migrations/2023_01_05_120942_create_chart_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chart_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid');
+            $table->string('slug');
+            $table->string('code');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('is_cash_source')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chart_accounts');
+    }
+};
